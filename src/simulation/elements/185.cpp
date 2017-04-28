3af9293c7e10b5a59b133b4a3052342200efaeb4
My main account <sf2164229838@sina.com>
2017-04-28 15:41:12 +0800

    Add files via upload

--- src/simulation/elements/185.cpp
+++ src/simulation/elements/185.cpp
@@ -139,7 +139,7 @@ int Element_E185::update(UPDATE_FUNC_ARGS)
 				{
 					parts[s].ctype = sctype;
 					parts[s].temp = parts[i].temp;
-					float veloc_multipler = (rand()%102 + 9950.0f) / 10000.0f
+					float veloc_multipler = (rand()%102 + 9950.0f) / 10000.0f;
 					parts[s].vx = parts[rr>>8].vx * veloc_multipler;
 					parts[s].vy = parts[rr>>8].vy * veloc_multipler;
 					if (sctype == PT_GRVT)
