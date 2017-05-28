82dfa99227fab38554715b0b2a760fec03691dd6
My main account <sf2164229838@sina.com>
2017-05-28 21:52:38 +0800

    Add files via upload

--- src/simulation/E189_update.cpp
+++ src/simulation/E189_update.cpp
@@ -1412,7 +1412,7 @@ int E189_Update::update(UPDATE_FUNC_ARGS)
 								rt = rr & 0xFF;
 								if (rt == PT_SPRK)
 									rt = parts[rr>>8].ctype;
-								if (rt && rt != PT_INWR && rt != PT_FILT && rt != PT_STOR)
+								if (rt && rt != PT_INWR && rt != PT_FILT && rt != PT_STOR && rt != PT_BIZR && rt != PT_BIZRG && rt != PT_BIZRS)
 									break;
 								pmap[ny][nx] = 0; // clear pmap
 								Element_PSTN::tempParts[rrx] = rr;
