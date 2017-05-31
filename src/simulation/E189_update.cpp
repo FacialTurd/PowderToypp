4957498844d658a75cedf21d9c837832b9fe8215
My main account <sf2164229838@sina.com>
2017-05-31 22:27:11 +0800

    Add files via upload

--- src/simulation/E189_update.cpp
+++ src/simulation/E189_update.cpp
@@ -1223,20 +1223,19 @@ int E189_Update::update(UPDATE_FUNC_ARGS)
 						r = pmap[y+ry][x+rx];
 						if (!r) continue;
 						pavg = sim->parts_avg(i,r>>8,PT_INSL);
-						if (pavg != PT_INSL && pavg != PT_INDI)
+						if (pavg == PT_INSL && pavg == PT_INDI)
+							continue;
+						if ((r & 0xFF) == PT_SPRK && parts[r>>8].life == 3)
 						{
-							if ((r & 0xFF) == PT_SPRK && parts[r>>8].life == 3)
-							{
-								parts[i].tmp2 = 2;
-								return return_value;
-							}
-							else if (rii == 2)
-							{
-								if ((r & 0xFF) == PT_INST)
-									sim->FloodINST(x+rx,y+ry,PT_SPRK,PT_INST);
-								else if (sim->elements[r & 0xFF].Properties & PROP_CONDUCTS)
-									conductTo (sim, r, x+rx, y+ry, parts);
-							}
+							parts[i].tmp2 = 2;
+							// return return_value;
+						}
+						else if (rii == 2)
+						{
+							if ((r & 0xFF) == PT_INST)
+								sim->FloodINST(x+rx,y+ry,PT_SPRK,PT_INST);
+							else if (sim->elements[r & 0xFF].Properties & PROP_CONDUCTS)
+								conductTo (sim, r, x+rx, y+ry, parts);
 						}
 					}
 			break;
