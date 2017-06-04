ec5ae07195a111368328bd9a270c805e35108a60
My main account <sf2164229838@sina.com>
2017-06-05 07:35:18 +0800

    Add files via upload

--- src/simulation/elements/182.cpp
+++ src/simulation/elements/182.cpp
@@ -1,25 +1,19 @@
 #include "simulation/Elements.h"
-//#TPT-Directive ElementClass Element_E182 PT_E182 182
+//#TPT-Directive ElementClass Element_POLO PT_POLO 182
 
 /*
 TODO: 
-	- Molten version
-	- "muted version" a powder created by mixing E182 with ? that is weaker
-	- "stronger version" a powder created by mixing E182 with E185 that is stronger?
+	- "stronger version" a powder created by mixing E182 (POLO) with E185 (???) that is stronger?
 */
 
-Element_E182::Element_E182()
+Element_POLO::Element_POLO()
 {
-	Identifier = "DEFAULT_PT_E182";
-	Name = "E182";
+	Identifier = "DEFAULT_PT_POLO";
+	Name = "POLO";
 	Colour = PIXPACK(0x506030);
 	MenuVisible = 1;
 	MenuSection = SC_NUCLEAR;
-#if (defined(DEBUG) || defined(SNAPSHOT)) && MOD_ID == 0
 	Enabled = 1;
-#else
-	Enabled = 0;
-#endif
 
 	Advection = 0.4f;
 	AirDrag = 0.01f * CFDS;
@@ -33,16 +27,16 @@ Element_E182::Element_E182()
 
 	Flammable = 0;
 	Explosive = 0;
-	Meltable = 0;
+	Meltable = 1;
 	Hardness = 0;
 
 	Weight = 90;
 
-	Temperature = R_TEMP+4.0f	+273.15f;
+	Temperature = 388.15f; 
 	HeatConduct = 251;
-	Description = "Experimental element. Some kind of nuclear fuel";
+	Description = "Polonium, highly radioactive. Decays into NEUT and heats up.";
 
-	Properties = TYPE_PART|PROP_NEUTPASS|PROP_RADIOACTIVE|PROP_LIFE_DEC;
+	Properties = TYPE_PART|PROP_NEUTPASS|PROP_RADIOACTIVE|PROP_LIFE_DEC |PROP_DEADLY;
 
 	LowPressure = IPL;
 	LowPressureTransition = NT;
@@ -50,14 +44,95 @@ Element_E182::Element_E182()
 	HighPressureTransition = NT;
 	LowTemperature = ITL;
 	LowTemperatureTransition = NT;
-	HighTemperature = ITH;
-	HighTemperatureTransition = NT;
+	HighTemperature = 526.95f;
+	HighTemperatureTransition = PT_LAVA;
 
-	Update = &Element_E182::update;
-	Graphics = &Element_E182::graphics;
+	Update = &Element_POLO::update;
+	Graphics = &Element_POLO::graphics;
 }
 
-//#TPT-Directive ElementHeader Element_E182 static int update(UPDATE_FUNC_ARGS)
+#define COOLDOWN 15
+#define LIMIT 5
+
+
+//#TPT-Directive ElementHeader Element_POLO static int update(UPDATE_FUNC_ARGS)
+int Element_POLO::update(UPDATE_FUNC_ARGS)
+{
+	int r = sim->photons[y][x];
+	if (parts[i].tmp < LIMIT && !parts[i].life)
+	{
+		if (!(rand()%10000) && !parts[i].tmp)
+		{
+			int s = sim->create_part(-3, x, y, PT_NEUT);
+			if (s >= 0)
+			{
+				parts[i].life = COOLDOWN;
+				parts[i].tmp++;
+
+				parts[i].temp = ((parts[i].temp + parts[s].temp) + 600.0f) / 2.0f;
+				parts[s].temp = parts[i].temp;
+			}
+		}
+
+		if (r && !(rand()%100))
+		{
+			int s = sim->create_part(-3, x, y, PT_NEUT);
+			if (s >= 0)
+			{
+				parts[i].temp = ((parts[i].temp + parts[r>>8].temp + parts[r>>8].temp) + 600.0f) / 3.0f;
+				parts[i].life = COOLDOWN;
+				parts[i].tmp++;
+
+				parts[r>>8].temp = parts[i].temp;
+
+				parts[s].temp = parts[i].temp;
+				parts[s].vx = parts[r>>8].vx;
+				parts[s].vy = parts[r>>8].vy;
+			}
+		}
+	}
+	if (parts[i].tmp2 >= 6)
+	{
+		sim->part_change_type(i,x,y,PT_PLUT);
+		parts[i].temp = (parts[i].temp+600.0f)/2.0f;
+		return 1;
+	}
+	if (parts[r>>8].type == PT_PROT)
+	{
+		parts[i].tmp2++;
+		sim->kill_part(r>>8);
+	}
+	else if (sim->isFromMyMod && (r & 0xFF) == PT_ELEC && !(rand()%25))
+	{
+		s = parts[i].tmp;
+		if (s) parts[i].tmp --;
+	}
+	if (parts[i].temp < 388.15f)
+	{
+		parts[i].temp += 0.2f;
+	}
+	return 0;
+}
+
+//#TPT-Directive ElementHeader Element_POLO static int graphics(GRAPHICS_FUNC_ARGS)
+int Element_POLO::graphics(GRAPHICS_FUNC_ARGS)
+{
+	if (cpart->tmp >= LIMIT)
+	{
+		*colr = 0x70;
+		*colg = 0x70;
+		*colb = 0x70;
+	}
+	else
+		*pixel_mode |= PMODE_GLOW;
+
+	return 0;
+}
+
+Element_POLO::~Element_POLO() {}
+
+
+/* # TPT-Directive ElementHeader Element_E182 static int update(UPDATE_FUNC_ARGS)
 int Element_E182::update(UPDATE_FUNC_ARGS)
 {
 	const int cooldown = 15;
@@ -102,17 +177,4 @@ int Element_E182::update(UPDATE_FUNC_ARGS)
 	return 0;
 }
 
-
-//#TPT-Directive ElementHeader Element_E182 static int graphics(GRAPHICS_FUNC_ARGS)
-int Element_E182::graphics(GRAPHICS_FUNC_ARGS)
-{
-	if(cpart->tmp >= 5) {
-		*colr = 0x70;
-		*colg = 0x70;
-		*colb = 0x70;
-	}
-
-	return 0;
-}
-
-Element_E182::~Element_E182() {}
+*/
