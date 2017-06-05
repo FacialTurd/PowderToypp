732804979475aa548141d58a6f6a5364c79fa1c6
My main account <sf2164229838@sina.com>
2017-06-05 08:34:22 +0800

    Update POLO.cpp

--- src/simulation/elements/POLO.cpp
+++ src/simulation/elements/POLO.cpp
@@ -74,7 +74,7 @@ int Element_POLO::update(UPDATE_FUNC_ARGS)
 			}
 		}
 
-		if (r && !(rand()%100 || sim->isFromMyMod && (r & 0xFF) != PT_ELEC))
+		if (r && !(rand()%100 || sim->isFromMyMod && (r & 0xFF) == PT_ELEC))
 		{
 			int s = sim->create_part(-3, x, y, PT_NEUT);
 			if (s >= 0)
