1f9e2bcec5130e206f46a9e91a75c0dba03dcf41
My main account <sf2164229838@sina.com>
2017-06-07 08:10:24 -0500

    Add files via upload

--- src/simulation/E189_update.cpp
+++ src/simulation/E189_update.cpp
@@ -189,7 +189,7 @@ int E189_Update::update(UPDATE_FUNC_ARGS)
 		{
 			for (rx=-1; rx<2; rx++)
 				for (ry=-1; ry<2; ry++)
-					if ((!rx != !ry) && BOUNDS_CHECK)
+					if ((rx || ry) && BOUNDS_CHECK)
 					{
 						r = pmap[y+ry][x+rx];
 						if (!r)
