f338fb2ff3ff3af57d640ddcdd1a0083c0d8c1ee
My main account <sf2164229838@sina.com>
2017-06-24 15:54:58 +0800

    Add files via upload

--- src/simulation/elements/189.cpp
+++ src/simulation/elements/189.cpp
@@ -368,7 +368,7 @@ void Element_MULTIPP::interactDir(Simulation* sim, int i, int x, int y, Particle
 				r1 = rct >> 6;
 				part_phot->vx = r1 * sim->portal_rx[rct&7];
 				part_phot->vy = r1 * sim->portal_ry[(rct>>3)&7];
-				part_other->tmp2 = r1<<6 | ((rct&7)<<3) | ((rct>>3)&7);
+				part_other->ctype = r1<<6 | ((rct&7)<<3) | ((rct>>3)&7);
 				break;
 		}
 	}
