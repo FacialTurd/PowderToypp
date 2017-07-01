3f52d5907228671a62063591364da04ab5bd1729
My main account <sf2164229838@sina.com>
2017-07-01 22:06:43 +0800

    Add files via upload

--- src/simulation/elements/NEUT.cpp
+++ src/simulation/elements/NEUT.cpp
@@ -176,7 +176,7 @@ int Element_NEUT::update(UPDATE_FUNC_ARGS)
 						sim->create_part(r>>8, x+rx, y+ry, PT_CAUS);
 					break;
 				case ELEM_MULTIPP:
-					if (parts[r>>8].tmp & 8)
+					if (parts[r>>8].life == 22 && parts[r>>8].tmp & 8)
 						parts[i].vx = 0, parts[i].vy = 0;
 					break;
 				default:
