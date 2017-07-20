c96385f74b940b71c6f8481a6077c7f3a0874915
My main account <sf2164229838@sina.com>
2017-07-20 11:35:03 +0800

    Add files via upload

--- src/simulation/MULTIPPE_update.cpp
+++ src/simulation/MULTIPPE_update.cpp
@@ -754,7 +754,7 @@ int MULTIPPE_Update::update(UPDATE_FUNC_ARGS)
 					if (BOUNDS_CHECK && (rx || ry))
 					{
 						r = pmap[y+ry][x+rx];
-						if ((r & 0xFF) == PT_SPRK && parts[r>>8].life == 3)
+						if ((r & 0xFF) == PT_SPRK && parts[r>>8].life == 3 || !(rx && ry) && sim->emap[(y+ry)/CELL][(x+rx)/CELL] >= 8)
 						{
 							parts[i].tmp2 = 10;
 							goto break2b;
