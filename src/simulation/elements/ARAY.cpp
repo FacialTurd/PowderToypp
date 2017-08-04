c6281ecce45303fa8f1b835aefbb2f5277d145ac
My main account <2976501934@qq.com>
2017-08-04 16:06:20 +0800

    Add files via upload

--- src/simulation/elements/ARAY.cpp
+++ src/simulation/elements/ARAY.cpp
@@ -219,6 +219,22 @@ int Element_ARAY::update(UPDATE_FUNC_ARGS)
 											nxx -= parts[r].tmp * nyi;
 										}
 										continue;
+									case 29: // Destroying FILT
+										{
+											int vtmp = parts[r].tmp + 2;
+											int rem1 = parts[r].tmp2;
+											int front1;
+											nyy += vtmp * nyi; nxx += vtmp * nxi;
+											while (sim->InBounds(x+nxx, y+nyy))
+											{
+												front1 = pmap[y+nyy][x+nxx];
+												if (rem1 && (--rem1)) break;
+												if ((front1&0xFF) == PT_FILT)
+													sim->kill_part(front1>>8);
+												else if (!rem1) break;
+											}
+										}
+										goto break1a;
 									}
 									docontinue = nostop;
 									continue;
